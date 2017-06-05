fd0e3c61a6e34e69c18a9d66f3e1e6e341c742fe
Natalie Thompson <nataliethompson@Keanons-MBP.att.net>
2017-06-05 15:50:15 -0500

    A more stylish game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,66 +15,7 @@ $(document).ready(function(){
  	document.getElementById("wrongCount").innerHTML= countW;
  	document.getElementById("rightCount").innerHTML= countR;
  	document.getElementById("timer").innerHTML= timer;
-
-//inserts question and shuffles button answer choices randomly 
-	function ranNum(choice, answers) {
-		document.getElementById("question").innerHTML = answers[0];
-	    var x = Math.floor(Math.random()*(random.length-1)+1);
-	    var y = random[x];
-		document.getElementById(choice).innerHTML= answers[y];
-	    random.splice(random.indexOf(y),1);
-	    return random;
-	    };
-	function populate (quest){   
-	ranNum("firstChoice", quest);
-	ranNum("secondChoice",quest);
- 	ranNum("thirdChoice",quest);
-	ranNum("fourthChoice",quest);
-};
-// populate(q2);
-//determines whether the correct answer was chosen and adds to the appr count
-function rightAnswer (choice, answers) {
-	$(choice).click(function(){
- 	if ($(choice).html()===(answers[1])){
- 		countR += 1;
-   		$("#rightCount").html(countR);
-   		// alert("Nice!");
-   		timer = 30;
-   		// random =[0,1,2,3,4];
-
-   	} else{
-   	countW += 1;
-   	$("#wrongCount").html(countW);
-   	// alert("Ouch! The correct answer was " + answers[1]);
-   	// stopTimer();
-   	timer = 30;
-   	// random =[0,1,2,3,4];
-
-   	// document.getElementById('timer').innerHTML = timer;
-   };
-});
-};
-$(".btn").mouseup(function(){
-    if(countW+countR===1)
-	{newQuestion(q2)}
-	else if(countW+countR===2) {
-	newQuestion(q3)}
-	else if(countW+countR===3) {
-	newQuestion(q4)};
-    });
-
-function areYouRight(ans){
-	rightAnswer("#firstChoice",ans);
-	rightAnswer("#secondChoice",ans);
-	rightAnswer("#thirdChoice",ans);
-	rightAnswer("#fourthChoice",ans);}
-
-function newQuestion(questionNum){
-	populate(questionNum);
-	areYouRight(questionNum);
-}
-
-	function timePlease (){
+ 		function timePlease (){
 
 		document.getElementById("start").onclick = function intervalCounter () {		
 			$("#start").hide();
@@ -92,60 +33,66 @@ function newQuestion(questionNum){
 				document.getElementById('timer').innerHTML =timer;
 			}, 1000);
 			newQuestion(q1);
-			
-	
-
 	}
 	}
 timePlease();
-// if((countW+countR===1)
-// {newQuestion(q2)}
-// else if((countW+countR===2) {
-// newQuestion(q3)}
-// else if((countW+countR===2) {
-// newQuestion(q4)};
+
+//inserts question and shuffles button answer choices randomly 
+	function ranNum(choice, answers) {
+		document.getElementById("question").innerHTML = answers[0];
+	    var x = Math.floor(Math.random()*(random.length-1)+1);
+	    var y = random[x];
+		document.getElementById(choice).innerHTML= answers[y];
+	    random.splice(random.indexOf(y),1);
+	    return random;
+	    };
+	function populate (quest){ 
+	random =[0,1,2,3,4];  
+	ranNum("firstChoice", quest);
+	ranNum("secondChoice",quest);
+ 	ranNum("thirdChoice",quest);
+	ranNum("fourthChoice",quest);
+};
+// populate(q2);
+//determines whether the correct answer was chosen and adds to the appr count
+function rightAnswer (choice, answers) {
+	$(choice).click(function(){
+ 	if ($(choice).html()===(answers[1])){
+ 		countR += 1;
+   		$("#rightCount").html(countR);
+   		alert("Nice!");
+   		timer = 30;
+   	
+
+   	} else{
+   	countW += 1;
+   	$("#wrongCount").html(countW);
+   	alert("Ouch! The correct answer was " + answers[1]);
+   	// stopTimer();
+   	timer = 30;
+   };
+   if(countW+countR===1)
+	{newQuestion(q2)}
+	else if(countW+countR===2) {
+	newQuestion(q3)}
+	else if(countW+countR===3) {
+	newQuestion(q4)};
+});
+};
 
 
-	
+function areYouRight(ans){
+	rightAnswer("#firstChoice",ans);
+	rightAnswer("#secondChoice",ans);
+	rightAnswer("#thirdChoice",ans);
+	rightAnswer("#fourthChoice",ans);}
+
+function newQuestion(questionNum){
+	populate(questionNum);
+	areYouRight(questionNum);
+}
+
+
+
    });
-
-
-
- 
-   
-		
-
-
-// $("#firstChoice").click(function(){
- // 	if ($("#firstChoice").html()===(q1[1]))
- //   		{countR += 1;
- //   		$("#rightCount").html(countR)}
-
- //   	else{
- //   		countW += 1;
- //   	$("#wrongCount").html(countW)}
- //   	});
- //   });
- // $(".btn").click(function(){
- 
- // 	if ($("#firstChoice").html()===(q1[1]))
- //   		{countR += 1;
- //   		$("#rightCount").html(countR)
-
- //   		console.log("Hit");
- //   	}
-
-   	// else{
-   	// 	countW += 1;
-   	// $("#wrongCount").html(countW)}
-   	// });
-
-
-
-
-
-
-//inner.html Question in first div "index 0"
-
-//inner.html words on button "index of 1/4"
 
